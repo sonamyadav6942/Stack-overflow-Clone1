4ad9bc3a81a0654e742ecc84ec3e3b7c2852baae
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2024-08-22 17:45:00 +0530

    pages added

---
+++ client/src/pages/Companies/Companies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Companies.css"
+const Companies = () => {
+  return (
+    <div className="companies">
+      <h1>Companies</h1>
+      <p>Learn about what it's like to work at companies</p>
+    </div>
+  );
+};
+
+export default Companies;
